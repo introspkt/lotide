ba5541d8e222a702dfce8b6678e3adc24aede639
Zarina <91272567+introspkt@users.noreply.github.com>
2022-06-27 21:45:12 -0400

    Create head.js

---
+++ head.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Asseration Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(array) {
+  if (array !== []) {
+    return array[0];
+  } else {
+    return undefined;
+  }
+};
+
+module.exports = head;

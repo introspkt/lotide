1a2be7709acefb3b9fd06ec2ec550b7fb979a242
Zarina <91272567+introspkt@users.noreply.github.com>
2022-07-04 13:24:20 -0400

    Create countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+    }
+  };
+  
+  const countLetters = function(str) {
+    let counter = {};
+  
+    for (let value of str) {
+      if (value === ' ') {
+        continue;
+      } else {
+        counter[value] = (counter[value] || 0) + 1;
+      }
+    }
+    return counter;
+  };
+  countLetters("now in the mix with DJ Introspkkt");
+  console.log(countLetters("now in the mix with DJ Introspkt"));
+  
+  module.exports = countLetters;
+
+

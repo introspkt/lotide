c31ffaafe089d6efccb3c8862ae7614c26e2b97e
Zarina <91272567+introspkt@users.noreply.github.com>
2022-07-05 20:52:19 -0400

    Create findKey.js

---
+++ findKey.js
@@ -0,0 +1,21 @@
+//assertEqual
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Asseration Failed: ${actual} !== ${expected}`);
+  }
+};
+
+module.exports = assertEqual;
+
+
+//findKey
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
